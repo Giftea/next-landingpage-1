5107ff5e209db362ffaed21fc264907390afd941
Uhiene Gift Eleojo <eleojogiftuhiene@gmail.com>
2021-07-03 22:43:30 +0100

    more on responsiveness

--- components/Banner.js
+++ components/Banner.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Image from 'next/image'
-import shape1 from "../assets/img/banner/shaps1.png";
-import shape2 from "../assets/img/banner/shaps2.png";
-import shape3 from "../assets/img/banner/shaps3.png";
-import shape4 from "../assets/img/banner/shaps4.png";
-import shape5 from "../assets/img/banner/shaps5.png";
-import shape6 from "../assets/img/banner/shaps6.png";
-import shape7 from "../assets/img/banner/shaps7.png";
 
 import bannerMoc from "../assets/img/banner/mockup.png";
 
--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,9 @@ import Banner from "../components/Banner";
 import FunFacts from "../components/FunFacts";
 import CtaTwo from "../components/CtaTwo";
 import CtaThree from "../components/CtaThree";
-import Video from "../components/Video";
 import Pricing from "../components/Pricing";
-import AppShots from "../components/AppShots";
 import Features from "../components/Features";
-import Testimonials from "../components/Testimonials";
 import BlogHome from "../components/BlogHome";
-import Clients from "../components/Clients";
 import CtaOne from "../components/CtaOne";
 import Footer from "../components/Footer";
 
@@ -26,11 +22,7 @@ const Home = () => {
       <FunFacts />
       <CtaTwo />
       <CtaThree />
-      {/* <Video /> */}
-      {/* <AppShots /> */}
       <Pricing />
-      {/* <Testimonials />
-      <Clients /> */}
       <BlogHome />
       <CtaOne />
       <Footer />
